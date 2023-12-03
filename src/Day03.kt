fef33d4e9c6dcd1c742910d80e59e9742ebafdc8
fagom <pradhan.subhasis174@gmail.com>
2023-12-03 15:26:50 +0530

    part 2 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,5 +1,4 @@
-import java.util.ArrayList
-
+//TODO: Refactor this puzzle code for efficiency. Code smells !!!
 fun main() {
     fun part1(input: List<String>): Int {
         var i = 0
@@ -15,7 +14,6 @@ fun main() {
                 val coordinate = schematic[j].trim()
                 // check for digits
                 if (coordinate.isNotEmpty() && coordinate[0].isDigit()) {
-
                     if (!symbolFound) {
                         // check for left bound symbols
                         if (j > 0 && schematic[j - 1].isNotEmpty() && isSymbol(schematic[j - 1])) {
@@ -137,12 +135,10 @@ fun main() {
                                 symbolFound = true
                             }
                         }
-
                     }
 
                     serialNumber += coordinate[0]
                     buildNumber = false
-                    //println("$symbolFound $serialNumber")
                     j++
                 } else {
                     j++
@@ -150,13 +146,8 @@ fun main() {
                 }
                 if (buildNumber) {
                     if (serialNumber != "" && symbolFound) {
-                        //println(Integer.parseInt(serialNumber))
                         sum += Integer.parseInt(serialNumber)
-                        //println(serialNumber)
                     }
-
-                    //val number = Integer.parseInt(serialNumber)
-                    //println(number)
                     serialNumber = ""
                     symbolFound = false
                 }
@@ -167,9 +158,11 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-
-
-        return input.size
+        val gears = mutableMapOf<Pair<Int, Int>, Pair<Int, Int>>()
+        findCoordinates(input, { it == '*' }) { number, symbol ->
+            gears[symbol] = gears[symbol]?.let { (n, m) -> n + 1 to m * number } ?: (1 to number)
+        }
+        return gears.values.asSequence().filter { it.first == 2 }.sumOf { it.second }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -182,6 +175,54 @@ fun main() {
     part2(input).println()
 }
 
+private infix fun IntRange.cartesian(other: IntRange): Sequence<Pair<Int, Int>> =
+    asSequence().flatMap { f -> other.asSequence().map { s -> f to s } }
+
+private fun findCoordinates(
+    records: List<String>,
+    adjacentCondition: (Char) -> Boolean,
+    action: (Int, Pair<Int, Int>) -> Unit,
+) {
+    records.forEachIndexed { i, line ->
+        var j = -1
+
+        while (++j < line.length) {
+            if (!line[j].isDigit()) continue
+
+            val numberString = line.substring(j).takeWhile { it.isDigit() }
+
+            val symbol = (i - 1..i + 1 cartesian j - 1..j + numberString.length)
+                .find { (a, b) -> records.getOrNull(a)?.getOrNull(b)?.let(adjacentCondition) == true }
+                ?: continue
+            action(numberString.toInt(), symbol)
+
+            j += numberString.length - 1
+        }
+    }
+}
+
+private fun inputBufferedReader() = System.`in`.bufferedReader()
+
+private fun getFullInput(): String = inputBufferedReader().readText().trimEnd()
+
+fun lines(): List<String> = mapLines { it }
+
+fun <T> mapLines(mapper: (String) -> T): List<T> =
+    inputBufferedReader().useLines { lines -> lines.map(mapper).toMutableList() }
+
+fun <T> mapBlocks(mapper: (List<String>) -> T): List<T> =
+    getFullInput().splitToSequence("\n\n").map { mapper(it.lines()) }.toMutableList()
+
+// Parsing utilities
+
+fun String.toInts(vararg delimiters: String = arrayOf(" ", ",")): List<Int> =
+    splitToSequence(*delimiters).filter { it.isNotEmpty() }.map { it.toInt() }.toMutableList()
+
+// Error handling utilities
+
+fun <T> T?.unwrap(): T = this ?: expect()
+
+fun expect(): Nothing = error("Invalid input / Unreachable")
 
 fun isSymbol(item: String): Boolean {
     val trimmedItem = item.trim()
